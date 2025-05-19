033012abf3db82c149e8b96078dec245761dcabc
ashishinfimatrix <ashirsat@infimatrix.com>
2025-05-19 15:24:20 +0530

    filter, footer and search page under working

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,8 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="flex justify-center items-center bg-gray-900 h-20 ">
+    <p className="text-gray-400">@ 2025 Eat Club. All Right Reserved</p>  
+    </div>
   )
 }
 
